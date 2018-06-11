5e62eb383fae5a2a861b4fbe7bc6d6ceaa6e1747
Alexis Espinoza <ajespiv@gmail.com>
2018-06-11 11:17:27 -0400

    Change in layout

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,29 @@
+class UrlsController < ApplicationController
+
+  def index
+    @urls = Url.all
+  end
+
+  def new
+    @url = Url.new
+  end
+
+  def create
+    @url = Url.new(url_params)
+    @url.save
+    redirect_to urls_path
+  end
+
+  def destroy
+    @url = Url.find(params[:id])
+    @url.destroy
+
+  redirect_to urls_path
+  end
+
+  private
+  def url_params
+    params.require(:url).permit(:name)
+  end
+
+end
